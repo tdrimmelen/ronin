21908688745f93af51282dd0912258bff2c2ced8
Theo van Drimmelen <theo@vandrimmelen.eu>
2025-02-16 17:10:38 +0100

    Possible solution for wrong chatgpt code

--- src/ronincontroller/controller.py
+++ src/ronincontroller/controller.py
@@ -19,11 +19,19 @@ class Controller:
         packet[0] = 0x0F  # S-Bus Start Byte
 
         # Insert channel data (11-bit values packed into bytes)
+        # This the original chatGPT code which I think is erroneous
         packet[1] = pan & 0xFF
         packet[2] = (pan >> 8) | ((tilt & 0x07) << 3)
         packet[3] = (tilt >> 3) & 0xFF
         packet[4] = (tilt >> 11) | ((roll & 0x3F) << 2)
         packet[5] = (roll >> 6) & 0xFF
+
+        # Alternative implementation. To be tried when other does not work
+        # packet[1] = pan & 0xFF
+        # packet[2] = (pan >> 8) | ((tilt & 0x1F) << 3)
+        # packet[3] = (tilt >> 5 ) & 0x3F
+        # packet[5] = (roll << 1) & 0xFF
+        # packet[6] = (roll >> 7) & 0xFF
 
         packet[24] = 0x00  # S-Bus End Byte
 
